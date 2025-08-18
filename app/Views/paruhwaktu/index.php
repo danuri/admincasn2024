df02225de6bb54fc50417d0843d15c47accc2ec4
danuri <danualbantani@gmail.com>
2025-08-18 17:55:43 +0700

    Update index.php

--- app/Views/paruhwaktu/index.php
+++ app/Views/paruhwaktu/index.php
@@ -97,7 +97,7 @@
                 </form>    
               </div>
               <div class="card-footer">
-                  <p><a href="<?= base_url('assets/template_sptjm_paruhwaktu.pdf')?>" target="_blank">Download Template</a></p>
+                  <p>Unggah surat usul dan SPTJM dalam 1 file. <a href="<?= base_url('assets/template_sptjm_paruhwaktu.pdf')?>" target="_blank">Download Template</a></p>
               </div>
             </div>
           </div>
