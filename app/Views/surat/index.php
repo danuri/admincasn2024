66a94ca6b682d74b6a4fe6054ff40e6147c41661
danuri <danualbantani@gmail.com>
2025-09-19 15:47:28 +0700

    Update index.php

--- app/Views/surat/index.php
+++ app/Views/surat/index.php
@@ -21,7 +21,7 @@
             <div class="card">
               <div class="card-body">
                 <div class="table-responsive">
-                  <table class="table align-middle table-striped-columns mb-0 datatable">
+                  <table class="table align-middle table-striped-columns mb-0 ">
                     <thead>
                       <tr>
                         <th>Jenis</th>
