110c333a003de1e7ccfb8e7899f35d3d341fb923
danuri <danualbantani@gmail.com>
2025-09-23 16:59:12 +0700

    cek nik

--- app/Controllers/Automate.php
+++ app/Controllers/Automate.php
@@ -113,7 +113,9 @@ class Automate extends BaseController
 
         curl_close($curl);
         $response = json_decode($response);
-        return $response;
+        // return $response;
+        // response json
+        return $this->response->setJSON($response);
 
     }
 }
