0f05de96859f080ff526f739342ff32826d19b94
danuri <danualbantani@gmail.com>
2025-09-25 12:32:08 +0700

    sdsd

--- app/Controllers/Automate.php
+++ app/Controllers/Automate.php
@@ -36,6 +36,8 @@ class Automate extends BaseController
             $cek = $this->checkNik($row->nik);
 
             $update = $model->update($row->id, ['pendidikan_lama'=>$cek->result->pendidikan, 'jabatan_lama'=>$cek->result->jabatan,'lokasi_lama'=>$cek->result->unor_id, 'is_sscasn' => '1']);
+        
+            return $this->response->setJSON($cek);
         }
     }
 
